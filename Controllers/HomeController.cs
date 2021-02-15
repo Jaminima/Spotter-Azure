c5a01318a34d9524e63c505d526b314d4eb138c2
Oscar Davies <oscar.william.davies@outlook.com>
2021-02-15 17:37:34 +0000

    Remove Template Code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace Spotter_Azure.Controllers
 {
+    [Route("")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -21,17 +22,6 @@ namespace Spotter_Azure.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
