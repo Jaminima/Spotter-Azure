d9271460d186cbec3e19650a12f97c303ecf77b9
Oscar Davies <oscar.william.davies@outlook.com>
2021-02-16 17:08:25 +0000

    Strip out old spotify key

--- SpotifyAuthKeys.cs
+++ SpotifyAuthKeys.cs
@@ -8,6 +8,6 @@ namespace Spotter_Azure
     {
         //https://accounts.spotify.com/authorize?client_id=edb0a4b7af91403db96c602fa2cc294f&response_type=code&redirect_uri=https://spotter-azure20210215171759.azurewebsites.net/api/register&scope=user-read-playback-state%20playlist-modify-public%20playlist-modify-private%20user-read-currently-playing%20user-library-modify%20user-read-playback-position%20playlist-read-private%20user-library-read
 
-        public static string client_id = "edb0a4b7af91403db96c602fa2cc294f", client_secret = "863b18119c324802999a3d8ec85fbf17";
+        public static string client_id = "None", client_secret = "None";
     }
 }
