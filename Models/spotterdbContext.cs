1d82fdb6ba9c985390f5d5c496fda2c22d01ab27
Oscar Davies <oscar.william.davies@outlook.com>
2021-02-20 18:26:04 +0000

    Change Hosting Structure

--- Models/spotterdbContext.cs
+++ Models/spotterdbContext.cs
@@ -30,7 +30,7 @@ namespace Spotter_Azure.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=spotter-sql.database.windows.net;Database=spotter-db;User Id=Jaminima; Password=William48");
+                optionsBuilder.UseSqlServer("Server=spotter-azuredbserver.database.windows.net;Database=Spotter-Azure_db;User Id=Jaminima; Password=William48");
             }
         }
 
